c33a54425b21f080874126750f83763268cc6870
Frederick <admin@freddyscovell.com>
2023-02-27 20:26:13 -0500

    a good starting point for my Tic-Tac-Toe app. The individual cards have the state hooked up correctly and they are aware of their location within the map

---
+++ src/Components/TCard.js
@@ -0,0 +1,31 @@
+// import Button from 'react-bootstrap/Button';
+// import Card from 'react-bootstrap/Card';
+import { useState } from 'react' 
+function TCard({numberOfCell}) {
+  const [ image, setImage ] = useState("https://www.tastingtable.com/img/gallery/why-capybara-was-once-considered-a-fish/intro-1662056291.jpg")
+  const [ temporaryNumber, setTemporaryNumber ] = useState(0)
+  return (
+    <div >
+      <img src={image} width="30%" height="30%" alt="Lettero" onClick={clicker}></img>
+    </div>
+  );
+
+  function clicker(){
+    console.log(numberOfCell)
+    if (temporaryNumber === 0){
+      setImage("https://pngimg.com/d/letter_o_PNG52.png")
+      setTemporaryNumber(temporaryNumber + 1)
+    }
+    else if (temporaryNumber === 1){
+      setImage("https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/LetterX.svg/1200px-LetterX.svg.png")
+      setTemporaryNumber(temporaryNumber + 1)
+    }
+    else{
+      setImage("https://www.tastingtable.com/img/gallery/why-capybara-was-once-considered-a-fish/intro-1662056291.jpg")
+      //  
+      setTemporaryNumber(0)
+    }
+  }
+}
+
+export default TCard;
