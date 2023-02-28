6cf4cecf74eade93e7d3b14ccd0d55805df1313a
Frederick <admin@freddyscovell.com>
2023-02-27 20:27:22 -0500

    a good starting point for my Tic-Tac-Toe app. The individual cards have the state hooked up correctly and they are aware of their location within the map

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
+// import TCard from './Components/TCard'
+import CardMapper from './Components/CardMapper'
 import './App.css';
-
+import { useState } from 'react' 
 function App() {
+  const [ numberOfCells, setNumberOfCells ] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Row justify-content = "center">
+          <CardMapper numberOfCells={numberOfCells}/>
+          {/* <Col>
+            <TCard/>
+          </Col>
+          <Col>
+            <TCard/>
+          </Col>
+          <Col>
+            <TCard/>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <TCard/>
+          </Col>
+          <Col>
+            <TCard/>
+          </Col>
+          <Col>
+            <TCard/>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <TCard/>
+          </Col>
+          <Col>
+            <TCard/>
+          </Col>
+          <Col>
+            <TCard/>
+          </Col> */}
+        </Row>
+
+      </Container>
+      <p>Freddy's Tic Tac Toe Game</p>
     </div>
   );
 }
